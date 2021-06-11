52147c0d00b18e6f09e64dbfa530c9f904997989
LBo <BodellLaura@gmail.com>
2021-06-11 11:57:04 -0400

    updated app.js file to include RecipesContainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import RecipesContainer from './components/RecipesContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React x
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <h1>Recipe List</h1>
+        </div>
+        <RecipesContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
