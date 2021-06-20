070f4d5cf3aa09b935dfebc64a42fce93d84ce3f
LBo <BodellLaura@gmail.com>
2021-06-19 20:42:57 -0400

    updated RecipeInput component to allow adding recipe action

--- src/components/RecipeInput.js
+++ src/components/RecipeInput.js
@@ -1,8 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { addRecipe } from '../actions/recipes';
+import { connect } from 'react-redux';
+
+export class RecipeInput extends Component {
+
+  state = {
+    title: '',
+    ingredients: '',
+    method: '',
+    time: '',
+    freezable: ''
+    
+  }
+
+  handleOnTitleChange = event => {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  handleOnIngredientsChange = event => {
+      this.setState({
+        ingredients: event.target.value
+      });
+    }
+
+  handleOnMethodChange = event => {
+    this.setState({
+      method: event.target.value
+    });
+  }
 
 
+  handleOnTimeChange = event => {
+      this.setState({
+        time: event.target.value
+      });
+    }
 
-      <h1>Recipes Input Page</h1>
- 
 
-export default RecipeInput;
+  handleOnFreezableChange = event => {
+      this.setState({
+        Freezable: event.target.value
+      });
+    }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.addRecipe(this.state);
+  }
+
+  render() {
+    return(
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnTitleChange(event)}
+            value={this.state.title}
+            placeholder="recipe name" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnIngredientsChange(event)}
+            value={this.state.ingredients}
+            placeholder="ingredients" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnMethodChange(event)}
+            value={this.state.method}
+            placeholder="method" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnTimeChange(event)}
+            value={this.state.time}
+            placeholder="time" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnFreezableChange(event)}
+            value={this.state.freezable}
+            placeholder="freezable?" />
+        </p>
+        <input type="submit" />
+      </form>
+    );
+  }
+};
+
+export default connect(null, { addRecipe })(RecipeInput);
